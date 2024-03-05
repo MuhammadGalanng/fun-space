3c7ec038f562841005bcba6fdd70ba77c9866f2f
Galang <brazzy3532717@gmail.com>
2024-03-05 10:53:54 +0700

    first commit

---
+++ Semes4/lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/const.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage ({super.key});
+
+  @override
+  State<HomePage> createstate () => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor;
+    )
+  }
+}
